53225cdea9a91f12009af3c111d7dfc1d0aaf41d
ratsata <s_dbz@hotmail.com>
2018-07-04 13:44:22 -0400

    newcamera/updatecamera

--- src/pages/list-camera/list-camera.ts
+++ src/pages/list-camera/list-camera.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { IonicPage,
         NavController,
         NavParams,
-        ModalController } from 'ionic-angular';
+        ModalController,
+        AlertController } from 'ionic-angular';
 
 import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
-/* import { AddDataPage } from '../add-data/add-data';
-import { EditDataPage } from '../edit-data/edit-data'; */
 
 @IonicPage()
 @Component({
@@ -18,11 +17,8 @@ import { EditDataPage } from '../edit-data/edit-data'; */
 export class ListCameraPage {
   private camara : any;
   
-  constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController, private sqlite: SQLite) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController, private sqlite: SQLite,public alertCtrl: AlertController) {
     this.camara = [];
-    /* let totalIncome = 0;
-    let totalExpense = 0;
-    let balance = 0; */
   }
 
   ionViewDidLoad() {
@@ -38,47 +34,77 @@ export class ListCameraPage {
       name: 'ionicdb.db',
       location: 'default'
     }).then((db: SQLiteObject) => {
-      db.executeSql('CREATE TABLE IF NOT EXISTS SV_CAMARA(id_camara INTEGER PRIMARY KEY, ds_nombre TEXT, ds_ip TEXT, ds_imagen TEXT, st_estado INT)', {})
+      db.executeSql('CREATE TABLE IF NOT EXISTS SV_CAMARA(id_camara INTEGER PRIMARY KEY, ds_nombre TEXT, ds_ip TEXT, ds_port TEXT, ds_usuario, ds_hash, ds_imagen TEXT, st_estado INT)', {})
       .then(res => console.log('Executed SQL'))
       .catch(e => console.log(e));
-      db.executeSql('SELECT * FROM SV_CAMARA ORDER BY id_camara DESC', {})
+      db.executeSql('SELECT * FROM SV_CAMARA  ORDER BY id_camara ASC', {})
       .then(res => {
         this.camara = [];
         for(var i=0; i<res.rows.length; i++) {
-          this.camara.push({id_camara:res.rows.item(i).id_camara,ds_nombre:res.rows.item(i).ds_nombre,ds_ip:res.rows.item(i).ds_ip,ds_imagen:res.rows.item(i).ds_imagen,st_estado:res.rows.item(i).st_estado})
+          this.camara.push({id_camara:res.rows.item(i).id_camara,
+                            ds_nombre:res.rows.item(i).ds_nombre,
+                            ds_ip:res.rows.item(i).ds_ip,
+                            ds_port:res.rows.item(i).ds_port,
+                            ds_usuario:res.rows.item(i).ds_usuario,
+                            ds_hash:res.rows.item(i).ds_hash,
+                            ds_imagen:res.rows.item(i).ds_imagen,
+                            st_estado:res.rows.item(i).st_estado
+          })
         }
       })
-      .catch(e => console.log(e));
-    }).catch(e => console.log(e));
+      .catch(e => console.log(JSON.stringify(e)));
+    }).catch(e => console.log(JSON.stringify(e)));
   }
-  
-  /* addData() {
-    this.navCtrl.push(AddDataPage);
-  }
-  
-  editData(rowid) {
-    this.navCtrl.push(EditDataPage, {
-      rowid:rowid
+
+  deleteConfirm(id,nombre) {
+    const confirm = this.alertCtrl.create({
+      title: 'Eliminar camara',
+      message: 'Esta seguro que quiere eliminar la camara: '+nombre+'?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: () => {
+            return;
+          }
+        },
+        {
+          text: 'Eliminar',
+          handler: () => {
+            this.deleteData(id);
+          }
+        }
+      ]
     });
-  } */
+    confirm.present();
+  }
   
-  deleteData(rowid) {
+  deleteData(id) {
+    console.log("1");
+    console.log(id);
+    console.log("2");
     this.sqlite.create({
       name: 'ionicdb.db',
       location: 'default'
     }).then((db: SQLiteObject) => {
-      db.executeSql('DELETE FROM expense WHERE rowid=?', [rowid])
+      //db.executeSql('UPDATE SV_CAMARA SET st_estado = 0 WHERE id_camara = ?',[id])
+      db.executeSql('DELETE FROM SV_CAMARA WHERE id_camara = ?',[id])
       .then(res => {
-        console.log(res);
+        console.log(JSON.stringify(res));
         this.getData();
       })
-      .catch(e => console.log(e));
-    }).catch(e => console.log(e));
+      .catch(e => console.log(JSON.stringify(e)));
+    }).catch(e => console.log(JSON.stringify(e)));
   }
 
-  addCamera() {
-		const modal = this.modalCtrl.create('NewCameraPage');
-		modal.present();
+  cameraModal(id="new") {
+    let addModal = this.modalCtrl.create('NewCameraPage', { id: id });
+    addModal.present();
+    addModal.onDidDismiss(item => {
+      /* if (item) {
+        this.camara.push(item);
+      } */
+      this.getData();
+    })
 	}
 
 }
--- src/pages/new-camera/new-camera.ts
+++ src/pages/new-camera/new-camera.ts
@@ -1,15 +1,9 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+
 import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 import { Toast } from '@ionic-native/toast';
-
-/**
- * Generated class for the NewCameraPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -20,33 +14,86 @@ export class NewCameraPage {
 
   isReadyToSave: boolean;
   form: FormGroup;
-  data = { ds_nombre:"", ds_ip:"", ds_imagen:"", st_estado:1 };
-  
+  MODAL_TITLE : string = 'CAMERA_ADD_TITLE';
+  camara : any = [];
+  id: string;
+
   constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController, formBuilder: FormBuilder,
     private sqlite: SQLite, private toast: Toast) {
     this.form = formBuilder.group({
-      profilePic: [''],
-      name: ['', Validators.required],
-      about: ['']
+      ds_nombre: ['', Validators.required],
+      ds_ip: ['', Validators.compose([
+        Validators.pattern(/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/),
+        Validators.required
+      ])],
+      ds_port: ['554', Validators.required],
+      ds_usuario: ['admin', Validators.required],
+      ds_hash: ['cleanvoltage2018', Validators.required],
+      ds_imagen: ['554', Validators.required]
     });
+    this.id = navParams.get('id');
+    if (this.id == 'new'){
+      this.MODAL_TITLE = "CAMERA_ADD_TITLE";
+    }else{
+      this.MODAL_TITLE = "CAMERA_UPDATE_TITLE";
+      this.getData(this.id);
+    }
 
-    // Watch the form for changes, and
     this.form.valueChanges.subscribe((v) => {
       this.isReadyToSave = this.form.valid;
     });
   }
 
-  saveData() {
+  validateIP(ipaddress) {  
+    if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipaddress)) {  
+      return (true)  
+    }
+    return (false)  
+  }
+
+  getData(id) {
     this.sqlite.create({
       name: 'ionicdb.db',
       location: 'default'
     }).then((db: SQLiteObject) => {
-      db.executeSql('INSERT INTO SV_CAMARA VALUES(NULL,?,?,?,?)',[this.data.ds_nombre,this.data.ds_ip,this.data.ds_imagen,this.data.st_estado])
+      db.executeSql('SELECT * FROM SV_CAMARA WHERE id_camara = ? AND st_estado = 1', [id])
+      .then(res => {
+        this.form.get('ds_nombre').setValue(res.rows.item(0).ds_nombre);
+        this.form.get('ds_ip').setValue(res.rows.item(0).ds_ip);
+        this.form.get('ds_port').setValue(res.rows.item(0).ds_port);
+        this.form.get('ds_usuario').setValue(res.rows.item(0).ds_usuario);
+        this.form.get('ds_hash').setValue(res.rows.item(0).ds_hash);
+        this.form.get('ds_imagen').setValue(res.rows.item(0).ds_imagen);
+      })
+      .catch(e => console.log(e));
+    }).catch(e => console.log(e));
+  }
+
+  saveData() {
+    if (!this.form.valid) { return; }
+    if (!this.validateIP(this.form.value.ds_ip)){
+      this.toast.show('IP Incorrecta, ingrese una valida', '5000', 'center').subscribe(
+        toast => { console.log("ERROR IP"); }
+      );
+      return;
+    }
+    this.sqlite.create({
+      name: 'ionicdb.db',
+      location: 'default'
+    }).then((db: SQLiteObject) => {
+      db.executeSql('INSERT INTO SV_CAMARA VALUES(NULL,?,?,?,?,?,?,?)',[this.form.value.ds_nombre,
+                                                                        this.form.value.ds_ip,
+                                                                        this.form.value.ds_port,
+                                                                        this.form.value.ds_usuario,
+                                                                        this.form.value.ds_hash,
+                                                                        this.form.value.ds_imagen,
+                                                                        1])
         .then(res => {
           console.log(res);
-          this.toast.show('Data saved', '5000', 'center').subscribe(
+          this.toast.show('Creación Exitosa!', '5000', 'center').subscribe(
             toast => {
-              this.navCtrl.popToRoot();
+              //this.navCtrl.popToRoot();
+              this.viewCtrl.dismiss(this.form.value);
             }
           );
         })
@@ -68,14 +115,55 @@ export class NewCameraPage {
     });
   }
 
+  updateData(id){
+    if (!this.form.valid) { return; }
+    if (!this.validateIP(this.form.value.ds_ip)){
+      this.toast.show('IP Incorrecta, ingrese una valida', '5000', 'center').subscribe(
+        toast => { console.log("ERROR IP"); }
+      );
+      return;
+    }
+    this.sqlite.create({
+      name: 'ionicdb.db',
+      location: 'default'
+    }).then((db: SQLiteObject) => {
+      db.executeSql('UPDATE SV_CAMARA SET ds_nombre = ?,ds_ip = ?,ds_port = ?,ds_usuario = ?,ds_hash = ?,ds_imagen = ? WHERE id_camara = ?',
+                                        [this.form.value.ds_nombre,
+                                          this.form.value.ds_ip,
+                                          this.form.value.ds_port,
+                                          this.form.value.ds_usuario,
+                                          this.form.value.ds_hash,
+                                          this.form.value.ds_imagen,
+                                        id])
+        .then(res => {
+          console.log(res);
+          this.toast.show('Guardado exitoso!', '5000', 'center').subscribe(
+            toast => {
+              //this.navCtrl.popToRoot();
+              this.viewCtrl.dismiss(this.form.value);
+            }
+          );
+        })
+        .catch(e => {
+          e = JSON.stringify(e);
+          this.toast.show(e, '5000', 'center').subscribe(
+            toast => {
+              console.log(toast);
+            }
+          );
+        });
+    }).catch(e => {
+      console.log(e);
+      this.toast.show(e, '5000', 'center').subscribe(
+        toast => {
+          console.log(toast);
+        }
+      );
+    });
+  }
+
   cancel() {
     this.viewCtrl.dismiss();
-  }
-  
-  done() {
-    if (!this.form.valid) { return; }
-    /* this.viewCtrl.dismiss(this.form.value); */
-    
   }
 
 }
